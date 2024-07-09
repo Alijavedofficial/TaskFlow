f915d389333dda2712200889c5de1d0ff548b4dd
Alijavedofficial <alijavedofficial0007@gmail.com>
2024-07-10 00:04:47 +0500

    Update:Context Logic

--- context/BoardContext/BoardContext.tsx
+++ context/BoardContext/BoardContext.tsx
@@ -16,7 +16,7 @@ const BoardContext = React.createContext({
 
 export const BoardProvider = ({children}:React.PropsWithChildren) => {
 
-    const [BoardState, dispatch] = React.useReducer(boardReducer, initialState)
+    const [BoardState, dispatch] = React.useReducer(boardReducer,boardInitialState )
     return(
         <BoardContext.Provider value={{BoardState, dispatch}}>
             {children}
@@ -30,13 +30,52 @@ function boardReducer(state: Board, action:BoardAction):Board {
             return action.payload
         }
         case 'MOVE_COLUMN': {
-            
+            const result = [...state.ordered]
+            const [removed] = result.splice(action.payload.source.index, 1)
+            result.splice(action.payload.destination.index, 0, removed)
+
+            const newState = {...state, ordered: result}
+            localStorage.setItem("@Board", JSON.stringify(newState))
+            return newState;
         }
         case 'MOVE_TASK': {
+            if(action.payload.source.droppableId === action.payload.destination.droppableId) {
+                const reorderedtasks = [
+                    ...state.columns[action.payload.source.droppableId],
+                ]
+                const [movedTask] = reorderedtasks.splice(action.payload.source.index, 1);
+                reorderedtasks.splice(action.payload.destination.index, 0, movedTask)
 
+                const newState = {
+                    ...state,columns: {
+                        ...state.columns, [action.payload.source.droppableId]: reorderedtasks
+                    }
+                }
+                localStorage.setItem("@Board", JSON.stringify(newState))
+                return newState;
+            }
+            const startTasks = [...state.columns[action.payload.source.droppableId]]
+            const finishTasks = [...state.columns[action.payload.destination.droppableId]]
+            const [removedTask] = startTasks.splice(action.payload.source.index, 1)
+            finishTasks.splice(action.payload.destination.index, 0 , removedTask)
+
+            const newState = {
+                ...state, 
+                columns: {
+                    ...state.columns, [action.payload.source.droppableId]: startTasks, [action.payload.destination.droppableId]: finishTasks
+                }
+            }
+            localStorage.setItem("@Board", JSON.stringify(newState))
+            return newState;
         }
         case 'ADD_TASK': {
-
+            const newState = {
+                ...state, columns: {
+                    ...state.columns, ["pending"]: [action.payload, ...state.columns.pending]
+                }
+            }
+            localStorage.setItem("@Board", JSON.stringify(newState))
+            return newState;
         }
         case 'REMOVE_TASK': {
 
